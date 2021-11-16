5920b209b0fb943e4bdb2a12a6c6e63a5f775196
billJackers <yuehengou007@gmail.com>
2021-11-15 17:57:05 -0800

    Started implementing Bishop legal moves

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bishop extends Piece {
@@ -14,6 +15,52 @@ public class Bishop extends Piece {
     }
 
     public List<Square> getLegalMoves() {
-        return null;
+        List<Square> legalMoves = new ArrayList<>();
+
+        int rank = this.parentSquare.getRank();
+        int file = this.parentSquare.getFile();
+
+        int rankSize = board.getRankSize();
+        int fileSize = board.getFileSize();
+
+        Square[] b = board.getBoard();
+
+        int pos = (fileSize*rank) + file;
+
+        int temp = 1;
+
+        // up and to the left
+        if (rank != 0 && file != 0) {
+            while (pos-(temp*fileSize)-temp % fileSize >= 0 && pos-(temp*fileSize)-temp >= 0 ) {
+                if (!b[pos-(temp*fileSize)-temp].hasPiece()) {
+                    legalMoves.add(b[pos-(temp*fileSize)-temp]);
+                }
+                if (b[pos-(temp*fileSize)-temp].hasPiece()) {
+                    if (this.canCapture(b[pos-(temp*fileSize)-temp])) {
+                        legalMoves.add(b[pos-(temp*fileSize)-temp]);
+                    }
+                    break;
+                }
+                temp++;
+            }
+        }
+
+        // up and to the right
+        if (rank != 0 && file < fileSize) {
+            while (pos-(temp*fileSize)+temp+1 % fileSize != 0 && pos-(temp*fileSize)+temp >= 0 ) {
+                if (!b[pos-(temp*fileSize)+temp].hasPiece()) {
+                    legalMoves.add(b[pos-(temp*fileSize)+temp]);
+                }
+                if (b[pos-(temp*fileSize)+temp].hasPiece()) {
+                    if (this.canCapture(b[pos-(temp*fileSize)+temp])) {
+                        legalMoves.add(b[pos-(temp*fileSize)+temp]);
+                    }
+                    break;
+                }
+                temp++;
+            }
+        }
+
+        return legalMoves;
     }
 }
