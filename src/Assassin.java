c3bd7562f9c663b67ca4c53a086ac33ecff0bec8
Ethan <thegreatergoo@gmail.com>
2021-11-18 10:56:57 -0800

    can you see this change

--- src/Assassin.java
+++ src/Assassin.java
@@ -25,68 +25,42 @@ public class Assassin extends Piece {
 
         int pos = (file*10) + rank;
 
-        /** Lines 29 to 66 define legal moves
-         * Since the movement options for the assassin are symmetrical it applies to blue and red
-         */
+        int column = pos % 10;
+        int row = pos / 10;
 
         int[] allMoves = {20, 9, 11, 2};  // [+] OR [-] these values RELATIVE TO OUR CURRENT INDEX gives us possible moves for the assassin
         for (int relativeMove : allMoves) {
             // if the relative moves are within the bounds of the board and the position is able to be captured, then add to legalMoves
-            if (pos + relativeMove < 100 && this.canCapture(b[pos + relativeMove])) legalMoves.add(b[pos + relativeMove]);
-            if (pos - relativeMove >= 0 && this.canCapture(b[pos - relativeMove])) legalMoves.add(b[pos - relativeMove]);
+            if (pos + relativeMove < 100 && this.canCapture(b[pos + relativeMove]) && acceptableRow(row) && acceptableColumn(column)) {
+                legalMoves.add(b[pos + relativeMove]);
+            }
+            if (pos - relativeMove >= 0 && this.canCapture(b[pos - relativeMove]) && acceptableRow(row) && acceptableColumn(column)) {
+                legalMoves.add(b[pos - relativeMove]);
+            }
         }
-
-
-
-
-        // Moves backwards 2 spaces
-        if (!b[pos + 20].hasPiece() && pos + 20 < 100) {
-            legalMoves.add(b[pos + 20]);
-        }
-        // Moves forward 2 spaces
-        if (!b[pos - 20].hasPiece() && pos - 20 >= 0) {
-            legalMoves.add(b[pos - 20]);
-        }
-
-        // Moves to the left 2 spaces
-        if (!b[pos - 2].hasPiece() && pos - 2 >= 0) {
-            legalMoves.add(b[pos - 2]);
-        }
-
-        // Moves to the left 2 spaces
-        if (!b[pos + 2].hasPiece() && pos + 2 < 100) {
-            legalMoves.add(b[pos + 2]);
-        }
-
-        // Moves left one space and up one space (one space diagonally)
-        if (!b[pos - 11].hasPiece() && pos - 11 >= 0) {
-            legalMoves.add(b[pos - 11]);
-        }
-
-        // Moves right one space and down one space (one space diagonally)
-        if (!b[pos + 11].hasPiece() && pos + 11 < 100) {
-            legalMoves.add(b[pos + 11]);
-        }
-
-        // Moves left one space and down one space (one space diagonally)
-        if (!b[pos + 9].hasPiece() && pos + 9 < 100) {
-            legalMoves.add(b[pos + 9]);
-        }
-
-        // Moves right one space and up one space (one space diagonally)
-        if (!b[pos - 9].hasPiece() && pos - 9 >= 0) {
-            legalMoves.add(b[pos - 9]);
-        }
-
-        // Captures
-        if (pos % 10 != 9 && pos+10 < 100 && b[pos+11].hasPiece() && this.canCapture(b[pos+11])) {
-            legalMoves.add(b[pos+11]);
-        }
-
-        if (pos % 10 != 0 && pos+9 < 100 && b[pos+9].hasPiece() && this.canCapture(b[pos+9])) {
-            legalMoves.add(b[pos+9]);
-        }
-
+        return legalMoves;
     }
 
+    public boolean acceptableRow(int row) {
+        int maxRow = row + 2;
+        int minRow = row - 2;
+            if (row <= maxRow && row >= minRow) {
+                return true;
+            }
+        return false;
+    }
+
+    public boolean acceptableColumn(int column) {
+        int maxColumn = column + 2;
+        int minColumn = column - 2;
+        if (column <= maxColumn && column >= minColumn) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<Square> getTargets() {
+        return null;
+    }
 }
