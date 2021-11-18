d9aeda816ebd5e21c5ac1dd61e94be5a9f73b876
Ethan <thegreatergoo@gmail.com>
2021-11-18 11:13:50 -0800

    Assassin is no longer ass

--- src/Assassin.java
+++ src/Assassin.java
@@ -31,29 +31,29 @@ public class Assassin extends Piece {
         int[] allMoves = {20, 9, 11, 2};  // [+] OR [-] these values RELATIVE TO OUR CURRENT INDEX gives us possible moves for the assassin
         for (int relativeMove : allMoves) {
             // if the relative moves are within the bounds of the board and the position is able to be captured, then add to legalMoves
-            if (pos + relativeMove < 100 && this.canCapture(b[pos + relativeMove]) && acceptableRow(row) && acceptableColumn(column)) {
+            if (pos + relativeMove < 100 && this.canCapture(b[pos + relativeMove]) && acceptableRow(row,(pos + relativeMove) / 10) && acceptableColumn(column,(pos + relativeMove) % 10)) {
                 legalMoves.add(b[pos + relativeMove]);
             }
-            if (pos - relativeMove >= 0 && this.canCapture(b[pos - relativeMove]) && acceptableRow(row) && acceptableColumn(column)) {
+            if (pos - relativeMove >= 0 && this.canCapture(b[pos - relativeMove]) && acceptableRow(row,(pos - relativeMove) / 10) && acceptableColumn(column,(pos - relativeMove) % 10)) {
                 legalMoves.add(b[pos - relativeMove]);
             }
         }
         return legalMoves;
     }
 
-    public boolean acceptableRow(int row) {
+    public boolean acceptableRow(int row, int proposedRow) {
         int maxRow = row + 2;
         int minRow = row - 2;
-            if (row <= maxRow && row >= minRow) {
+            if (proposedRow <= maxRow && proposedRow >= minRow) {
                 return true;
             }
         return false;
     }
 
-    public boolean acceptableColumn(int column) {
+    public boolean acceptableColumn(int column, int proposedColumn) {
         int maxColumn = column + 2;
         int minColumn = column - 2;
-        if (column <= maxColumn && column >= minColumn) {
+        if (proposedColumn <= maxColumn && proposedColumn >= minColumn) {
             return true;
         }
         return false;
