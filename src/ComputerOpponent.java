d191de16e6cea28a19ad84f7250d9444db7f09a6
Gabriel Rosas <TheRealTheriel@gmail.com>
2021-12-07 21:22:58 -0800

    created a class for the AI

---
+++ src/ComputerOpponent.java
@@ -0,0 +1,5 @@
+public class ComputerOpponent {
+    public ComputerOpponent(Board board) {
+
+    }
+}
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -12,22 +12,18 @@ public class GameWindow {
         JFrame gameWindow = new JFrame("Giga Chess");
         gameWindow.setLocationRelativeTo(null);
 
-        Board board = new Board(settings);
+        Board board = new Board(settings);  // creates the board JPanel to draw on
+        board.getController().setClocks(hours, minutes, seconds, increment);  // setting our clocks
+        StatsDisplay stats = new StatsDisplay(board, hours, minutes, seconds);  // stats displayer JPanel
+        ComputerOpponent computerOpponent = new ComputerOpponent(board);  // creating the AI opponent
 
-        board.getController().setClocks(hours, minutes, seconds, increment);
-
-        StatsDisplay stats = new StatsDisplay(board, hours, minutes, seconds);  // stats displayer panel
+        // adding JPanels to JFrame
         gameWindow.add(stats, BorderLayout.NORTH);
-
-        //gameWindow.add(stats, BorderLayout.NORTH); // creates the stats JPanel to display the games statistics above the board panel
-        gameWindow.add(board, BorderLayout.SOUTH); // creates the board JPanel to draw on. This also initializes the game loop
+        gameWindow.add(board, BorderLayout.SOUTH);
 
         gameWindow.setSize(board.getPreferredSize()); // Set the size of the window based on the size of the board
-
         gameWindow.setResizable(false); // don't allow the user to resize the window
         gameWindow.pack(); // pack() should be called after setResizable() to avoid issues on some platforms
-
-
         gameWindow.setVisible(true);
         gameWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
