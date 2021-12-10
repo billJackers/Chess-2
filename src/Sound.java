557c4f98b61f1f0acce917a12af1abf3f879fa27
billJackers <yuehengou007@gmail.com>
2021-12-10 10:36:42 -0800

    Fixed sound bug

--- src/Sound.java
+++ src/Sound.java
@@ -10,7 +10,7 @@ public class Sound {
 
     public Sound(String soundLocation) {
         try {
-            File soundPath = new File(soundLocation);
+            soundPath = new File(soundLocation);
             if (soundPath.exists()) {
                 audioInput = AudioSystem.getAudioInputStream(soundPath);
                 clip = AudioSystem.getClip();
