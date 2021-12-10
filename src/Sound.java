2cf9a0901c843098edcd99897124043bb131b807
leejustin01 <justlee0308@gmail.com>
2021-12-09 19:08:36 -0800

    Sound effects work on piece movement

---
+++ src/Sound.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.sql.SQLOutput;
+import javax.sound.sampled.*;
+public class Sound {
+
+    private File soundPath;
+    private Clip clip;
+    private boolean valid = true;
+    private AudioInputStream audioInput;
+
+    public Sound(String soundLocation) {
+        try {
+            File soundPath = new File(soundLocation);
+            if (soundPath.exists()) {
+                audioInput = AudioSystem.getAudioInputStream(soundPath);
+                clip = AudioSystem.getClip();
+            } else {
+                System.out.println("Can't find audio file at: " + soundLocation);
+                valid = false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            valid = false;
+        }
+    }
+
+    public void play() {
+        if (valid) {
+            try {
+                this.clip.open(audioInput);
+                this.clip.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
