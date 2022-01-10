8182dcb7f0a26b2fcc5a8a6dd41c0499f78b52c5
billJackers <yuehengou007@gmail.com>
2022-01-09 21:40:49 -0800

    Simplified the Sound class a little bit

--- src/Sound.java
+++ src/Sound.java
@@ -12,7 +12,7 @@ public class Sound {
         try {
             File soundPath = new File(soundLocation);
             if (soundPath.exists()) {
-                audioInput = AudioSystem.getAudioInputStream(new File(soundLocation).getAbsoluteFile());
+                audioInput = AudioSystem.getAudioInputStream(soundPath.getAbsoluteFile());
                 AudioFormat format = audioInput.getFormat();
                 DataLine.Info info = new DataLine.Info(Clip.class, format);
                 clip = (Clip) AudioSystem.getLine(info);
@@ -29,9 +29,6 @@ public class Sound {
     public void play() {
         if (valid) {
             try {
-                // FloatControl for volume, doesn't work
-                // FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-                // gainControl.setValue(volume);
                 this.clip.open(audioInput);
                 this.clip.start();
             } catch (Exception e) {
