b1a77e491968c003179996c94b30e0d0aa3fe1e8
billJackers <yuehengou007@gmail.com>
2022-01-17 20:55:52 -0800

    Nerfed archers so they can't shoot pawns

--- src/SideDisplay.java
+++ src/SideDisplay.java
@@ -9,11 +9,13 @@ public class SideDisplay extends JPanel {
     private final int WIDTH;
     private final Board board;
     private Settings settings;
+    private PlayerController controller;
 
     public SideDisplay(Board board, Settings settings) {
 
         this.board = board;
         this.settings = settings;
+        this.controller = board.getController();
 
         this.HEIGHT = board.getHeight() + board.getHeight() / 10;
         this.WIDTH = board.getWidth() / 3;
@@ -21,13 +23,28 @@ public class SideDisplay extends JPanel {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT)); // dimensions based on the size of the board
         this.setMaximumSize(this.getPreferredSize());
 
+        // Emote buttons
         JButton lolBtn = new JButton("lol");
         lolBtn.addActionListener(e -> {
             Sound lol = new Sound("src/sounds/lol.wav");
             lol.play();
         });
 
+        JButton bruhBtn = new JButton("bruh");
+        bruhBtn.addActionListener(e -> {
+            Sound bruh = new Sound("src/sounds/bruh.wav");
+            bruh.play();
+        });
+
+        JButton painBtn = new JButton("pain");
+        painBtn.addActionListener(e -> {
+            Sound pain = new Sound("src/sounds/pain.wav");
+            pain.play();
+        });
+
         this.add(lolBtn);
+        this.add(bruhBtn);
+        this.add(painBtn);
 
         this.setVisible(true);
 
@@ -40,4 +57,6 @@ public class SideDisplay extends JPanel {
     public int getWidth() {
         return WIDTH;
     }
+
+
 }
